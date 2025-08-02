7533cc2594ce599b636305db68e2da4c7358c3f1
Yarin Tzdaka <yarintz33@gmail.com>
2025-08-02 16:06:04 +0300

    change cors origin

--- bookService/src/main/java/com/yarin/bookService/controller/BookController.java
+++ bookService/src/main/java/com/yarin/bookService/controller/BookController.java
@@ -4,16 +4,11 @@ import com.yarin.bookService.model.Book;
 import com.yarin.bookService.service.BookService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
-@CrossOrigin(origins = "*")
 public class BookController {
     
     private final BookService bookService;
